024ffd85e00438deb69401b74778e02bf3c50ec4
Vaibhav Shringarpure <vibs.sh@gmail.com>
2019-06-15 22:50:15 +0100

    Tidy and Cleanup the Code

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,7 +6,8 @@ import axios from 'axios';
 (function () {
   'use strict';
 
-  console.log('App Launched'); // To make sure the everything is wired
+  // To make sure the everything is wired
+  console.log('App Launched'); 
 
   //Cache the Variables in settings
   const settings = {
@@ -16,58 +17,34 @@ import axios from 'axios';
     apiURL: 'https://api.myjson.com/bins/jw3rg',
     loader: document.querySelector('#loader')
   }
-
-  
+ 
   //Construct Accordion Here
   const constructAccordion = function(data) {
     return (
        `<div class="accordion-wrapper">
             <h3>
-              <button class="accordion-trigger" aria-expanded="false" aria-control="accordion-${data.id}" data-trigger="accordion-${data.id}">
-                <span class="button-text" data-trigger="accordion-${data.id}">
-                  Q:${data.question}
-                </span>
-                <span class="button-icon" data-trigger="accordion-${data.id}"></span>
+              <button class="accordion-trigger" 
+                      aria-expanded="false" 
+                      aria-control="accordion-${data.id}" 
+                      data-trigger="accordion-${data.id}"
+              >
+              <span class="button-text" data-trigger="accordion-${data.id}">
+                Q:${data.question}
+              </span>
+              <span class="button-icon" data-trigger="accordion-${data.id}"></span>
               </button>
             </h3> 
             
-            <div class="accordion-panel" role="region"  aria-labelledby="accordion-${data.id}" data-panel="accordion-${data.id}" hidden>
+            <div class="accordion-panel" 
+                role="region"  
+                aria-labelledby="accordion-${data.id}" 
+                data-panel="accordion-${data.id}" 
+                hidden
+            >
               <p>${data.answer}</p>
             </div>
           </div> `
       )
-  }
-
-
-  //Get Dynamic Data from API
-  const getData = function () {
-    axios.get(settings.apiURL)
-      .then(function(response) {
-        const faqs = response.data.faqs; // Get all Faqs
-        dataLooper(faqs);
-      })
-      .catch(function(err) {
-        console.log(err);
-      })
-  }
-
-  //Loop over the data and create Accordion
-  const dataLooper = function(data) {
-    data.forEach(function (d) {
-    //Get Accordion Created
-      const accordion = constructAccordion(d);
-
-      //Remove Loader Gif 
-      settings.loader.style.display = "none";
-
-      //Inject Accordion into the DOM
-      settings.accordionContainer.innerHTML += accordion;
-
-      //Making first Accordion Visible
-      firstElementVisible();
-
-
-    })
   }
 
   //Set First Element of the accordion to expanded on load
@@ -79,6 +56,36 @@ import axios from 'axios';
     const dataAttribute = firstTrigger.getAttribute('data-trigger');
     //panel
     panelToggle(firstTrigger, dataAttribute)
+  }
+
+  //Loop over the data and create Accordion
+  const dataLooper = function(data) {
+    data.forEach(function (d) {
+      //Get Accordion Created
+      const accordion = constructAccordion(d);
+
+      //Hide Loader Gif 
+      settings.loader.setAttribute('aria-hidden', 'true');
+
+      //Inject Accordion into the DOM
+      settings.accordionContainer.innerHTML += accordion;
+
+      //Making first Accordion Visible
+      firstElementVisible();
+      
+    })
+  }
+
+  //Get Dynamic Data from API
+  const getData = function () {
+    axios.get(settings.apiURL)
+      .then(function(response) {
+        const faqs = response.data.faqs; // Get all Faqs
+        dataLooper(faqs);
+      })
+      .catch(function(err) {
+        console.log(err);
+      })
   }
 
   //Panel Toggle
@@ -99,7 +106,7 @@ import axios from 'axios';
   //Add EventListeners 
   const bindUIActions = function() {
     //Loader gif display
-    settings.loader.style.display = "block"; 
+    settings.loader.setAttribute('aria-hidden', 'false');
 
     //Click Even on the Trigger
     settings.accordionContainer.addEventListener('click', function (event) {
@@ -113,9 +120,9 @@ import axios from 'axios';
 
       //Toggling the Aria-expanded attribute
       if(ariaExpanded === 'true') {
-          ariaExpanded = 'false'
+          ariaExpanded = 'false';
         } else {
-          ariaExpanded = 'true'
+          ariaExpanded = 'true';
       }
       
       //setting all Triggers to Aria-Expanded false
@@ -124,21 +131,20 @@ import axios from 'axios';
       })
 
       // Clicked Accordion Trigger
-      event.target.setAttribute('aria-expanded', ariaExpanded)
+      event.target.setAttribute('aria-expanded', ariaExpanded);
 
       //Panel
-      panelToggle(event.target, dataAttrib)
-      
+      panelToggle(event.target, dataAttrib);      
 
     },false)
   }
 
+  //Public API
   const init = () => {    
     getData();
     bindUIActions();
   }
 
-  init();
- 
+  init(); 
 
 }());
